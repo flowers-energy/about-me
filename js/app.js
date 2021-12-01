a58a4b7da279fa66445635fd53e51658aacfabbc
flowers-energy <zacharyflowers@zacharys-MBP.wavecable.com>
2021-11-30 19:06:43 -0800

    lab 2 build 1 unfinished aside and lists

---
+++ js/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+console.log('hello world');
+
+
+// user name question
+let user = prompt('HI!' + ' What is your name?');
+      console.log('user name is' + user);
+alert('Excellent name, ' + user + '! Now, let\'s talk about me.');
+      
+// question one correct answer yes
+let answerOne = prompt('Did I serve in the military?');
+if (answerOne === 'yes' || answerOne === 'y');
+      //console.log('Yes, I did some time in the Navy.');
+      alert('Yes, I did some time in the Navy!');
+
+let answerTwo = prompt('Do I have a dog?');
+if (answerTwo === 'yes' || answerTwo === 'y');
+      //console.log('Indeed, her name is Phoebe.');
+      alert('Indeed, her name is Phoebe!');
+
+let answerThree = prompt('Did I go to College?');
+if (answerThree === 'yes' || answerThree === 'y');
+      //console.log('Of course, I went to The Academy of Contemporary Music @ the University of Central Oklahoma. And yes, it is a mouthful.);
+      alert('Of course, I went to The Academy of Contemporary Music @ the University of Central Oklahoma. And yes, it is a mouthful.');
+
+let answerFour = prompt('Did I live in Oklahoma?');
+if (answerFour === 'yes' || answerFour === 'y');
+//console.log('Yes. I don't recommend it.');
+alert('Yes. I don\'t recommend it.');
+
+let answerFive = prompt('Do I like music?')
+if (answerFive === 'yes' || answerFive === 'y');
+      //console.log('Oh most definitely.');
+      alert('Oh most definitely. Thanks ' +user+ ' for stopping by.');
+
+
+
