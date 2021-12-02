c2b8b0935971ba2d1e27a1db99741531502fd9c7
flowers-energy <zachary.t.flowers@gmail.com>
2021-12-02 15:28:45 -0800

    updated js with new numGame

--- js/app.js
+++ js/app.js
@@ -5,50 +5,104 @@ console.log('hello world');
 
 // user name question
 let user = prompt('HI!' + ' What is your name?');
-      console.log('user name is' + user);
+console.log('user name is ' + user);
 alert('Excellent name, ' + user + '! Now, let\'s talk about me.');
-      
+
+
+let score = 7;
+
+
 // question one correct answer yes
-let answerOne = prompt('Did I serve in the military?');
-if (answerOne === 'yes' || answerOne === 'y');
+let answerOne = prompt('Did I serve in the military?').toLowerCase();
+if (answerOne === 'yes' || answerOne === 'y') {
       //console.log('Yes, I did some time in the Navy.');
       alert('Yes, I did some time in the Navy!');
 
-let answerTwo = prompt('Do I have a dog?');
-if (answerTwo === 'yes' || answerTwo === 'y');
+} else {
+      alert('Hmm I did serve in the navy.');
+      score--;
+}
+
+let answerTwo = prompt('Do I have a dog?').toLowerCase();
+if (answerTwo === 'yes' || answerTwo === 'y') {
       //console.log('Indeed, her name is Phoebe.');
       alert('Indeed, her name is Phoebe!');
 
-let answerThree = prompt('Did I go to College?');
-if (answerThree === 'yes' || answerThree === 'y');
+} else {
+      alert('Well I do have a dog and Phoebe is her name.');
+      score--;
+
+}
+let answerThree = prompt('Did I go to College?').toLowerCase();
+if (answerThree === 'yes' || answerThree === 'y') {
       //console.log('Of course, I went to The Academy of Contemporary Music @ the University of Central Oklahoma. And yes, it is a mouthful.);
       alert('Of course, I went to The Academy of Contemporary Music @ the University of Central Oklahoma. And yes, it is a mouthful.');
 
-let answerFour = prompt('Did I live in Oklahoma?');
-if (answerFour === 'yes' || answerFour === 'y');
-//console.log('Yes. I don't recommend it.');
-alert('Yes. I don\'t recommend it.');
+} else {
+      alert(`Nope, I am a learned individual.`);
+      score--;
+}
 
-let answerFive = prompt('Do I like music?')
-if (answerFive === 'yes' || answerFive === 'y');
+let answerFour = prompt('Did I live in Oklahoma?').toLowerCase();
+if (answerFour === 'yes' || answerFour === 'y') {
+      //console.log('Yes. I don't recommend it.');
+      alert('Yes. I don\'t recommend it.');
+
+} else {
+      alert(`Nope, I am sooner born and sooner bred... I guess.`);
+      score--;
+
+}
+let answerFive = prompt('Do I like music?').toLowerCase();
+if (answerFive === 'yes' || answerFive === 'y') {
       //console.log('Oh most definitely.');
       alert('Oh most definitely.');
 
+} else {
+      alert(`Do you even shoegaze?`);
+      score--;
+}
 
-let attemptsRemain = 3;
-let indexCount = 0;
 
-let numGame = prompt(`${user}, what number am I thinking? You have four guesses.`);
- for (let i = 0; i < 3; i++){
-       if (numGame !== 42){
-             alert('Please try again.');
-       }
+function guessGame() {
+      let attemptsRemain = 3;
+      let bingo = 42;
+      let i;
+      for (i = 0; i <= attemptsRemain; i++) {
+            let numGame = prompt(`${user}, what number am I thinking? You have four guesses.`);
 
-       if (numGame == 42){
-             alert('Bingo, you must have read my mind!')
-       }
- }
+            console.log(numGame)
+            console.log(i)
 
+            if (numGame == bingo) {
+                  alert('Bingo, you must have read my mind!');
+                  break;
+
+            } else if (numGame > bingo) {
+                  alert(`That guess is too high ${user}. Try again`);
+                  //      attemptsRemain--;
+                  score--;
+
+            } else if (numGame < bingo) {
+                  alert(`That guess is too low ${user}. Try again`);
+                  //      attemptsRemain--;
+                  score--;
+
+            } if (i == 2) {
+                  alert('Hint: It is the answer to the ultimate question of life, the universe, and everything.');
+
+            } if (i == attemptsRemain) {
+                  alert(`Sorry ${user}. You are out of attempts. The number I was thinking was 42.`);
+                  //break;
+
+            }
+      }
+}
+
+guessGame()
+
+
+console.log(score);
 
 //while (let i = 0; i < 3 );{
 
@@ -57,11 +111,9 @@ let numGame = prompt(`${user}, what number am I thinking? You have four guesses.
 //}
 //let attempts = [0,1,2,3,4,5,6,]
 //for (let i = 0; i > attempts;){ 
-      
+
 //}
 
 
       //console.log('You must have read my mind!');
       //if (answerSix === '42');
-
-
